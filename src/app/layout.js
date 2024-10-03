c0e61bd709f3d0d02eff7891997187cfca549309
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-10-03 14:11:19 +0000

    gellix font added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
+const gellixRegular = localFont({
+  src: "./fonts/Gellix-Regular.ttf",
+  display: "swap",
+});
 export const metadata = {
   title: "Botify",
   description:
@@ -21,11 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={gellixRegular.className}>{children}</body>
     </html>
   );
 }
