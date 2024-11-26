726dfa2ad554fd1fc06c87d72a96b6de7ad606ec
Samuel Yeboah <khobewayne@gmail.com>
2024-11-26 14:10:53 +0000

    redirect to dashboard after successful chatbot creation

--- src/app/dashboard/new/page.jsx
+++ src/app/dashboard/new/page.jsx
@@ -11,10 +11,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { chatbotSchema } from "@/lib/schema";
 import { LoadingSpinner } from "@/components/ui/loading-spinner";
+import SettingUpChatbot from "@/components/misc/setting-up";
 
 function Page() {
+  //handle navigation after successful adding c
+
   //step state
   const [step, setStep] = useState(1);
+
   const totalSteps = 2;
 
   //file state
@@ -34,6 +38,9 @@ function Page() {
     try {
       const res = await axios.post("/api/routes/new", data);
       console.log(res);
+      if (res.status === 200) {
+        router.push("/dashboard");
+      }
     } catch (error) {
       console.log("error creating new chatbot", error);
     }
@@ -55,69 +62,77 @@ function Page() {
   //routing
   const router = useRouter();
   return (
-    <div className="flex flex-col w-full h-full overflow-hidden gap-5">
-      {/* Header */}
-      <div className="header py-6 px-12 border-b border-b-gray-200">
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex flex-row gap-1 items-center">
-            <ArrowLeft
-              className="text-gray-500 hover:cursor-pointer"
-              onClick={() => router.back()}
-            />
-            <h3 className="text-gray-900 text-lg leading-8 font-bold">
-              Create a new chatbot
-            </h3>
+    <>
+      {isSubmitting ? (
+        <div className="flex w-screen h-screen items-center justify-center">
+          <SettingUpChatbot />
+        </div>
+      ) : (
+        <div className="flex flex-col w-full h-full overflow-hidden gap-5">
+          {/* Header */}
+          <div className="header py-6 px-12 border-b border-b-gray-200">
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-1 items-center">
+                <ArrowLeft
+                  className="text-gray-500 hover:cursor-pointer"
+                  onClick={() => router.back()}
+                />
+                <h3 className="text-gray-900 text-lg leading-8 font-bold">
+                  Create a new chatbot
+                </h3>
+              </div>
+            </div>
+          </div>
+          {/* Preview*/}
+          {/* <Step1 /> */}
+          {/* <Step2 /> */}
+          <div className="flex-grow px-12">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {step === 1 && <Step1 register={register} errors={errors} />}
+              {step === 2 && <Step2 register={register} errors={errors} />}
+            </form>
+          </div>
+
+          {/* Steps */}
+          <div className="">
+            <section className="flex flex-row justify-between px-12 py-6">
+              <div className="flex flex-col gap-2">
+                <div className="text-gray-500 text-sm">step {step} of 2</div>
+                <Stepper totalSteps={totalSteps} currentStep={step} />
+              </div>
+              <div className="flex flex-row gap-4">
+                <CustomButton
+                  className={`bg-white text-blue-600 border border-blue-600 ${
+                    step === 1
+                      ? "bg-gray-200 border-none text-white cursor-not-allowed"
+                      : "bg-white"
+                  }`}
+                  onClick={handlePrevStep}
+                  disabled={step === 1}
+                >
+                  Go back
+                </CustomButton>
+                <CustomButton
+                  onClick={() => {
+                    handleNextStep();
+                    step === 2 && handleFinish();
+                  }}
+                  disabled={step === totalSteps || isSubmitting}
+                  className={`${step === 2 && "px-6"} ${
+                    isSubmitting
+                      ? "bg-gray-200 border-none text-white cursor-not-allowed"
+                      : ""
+                  }`}
+                >
+                  {isSubmitting && <LoadingSpinner />}
+                  {step === 2 ? "Finish" : "Continue"}
+                </CustomButton>
+              </div>
+            </section>
           </div>
         </div>
-      </div>
-      {/* Preview*/}
-      {/* <Step1 /> */}
-      {/* <Step2 /> */}
-      <div className="flex-grow px-12">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {step === 1 && <Step1 register={register} errors={errors} />}
-          {step === 2 && <Step2 register={register} errors={errors} />}
-        </form>
-      </div>
-
-      {/* Steps */}
-      <div className="">
-        <section className="flex flex-row justify-between px-12 py-6">
-          <div className="flex flex-col gap-2">
-            <div className="text-gray-500 text-sm">step {step} of 2</div>
-            <Stepper totalSteps={totalSteps} currentStep={step} />
-          </div>
-          <div className="flex flex-row gap-4">
-            <CustomButton
-              className={`bg-white text-blue-600 border border-blue-600 ${
-                step === 1
-                  ? "bg-gray-200 border-none text-white cursor-not-allowed"
-                  : "bg-white"
-              }`}
-              onClick={handlePrevStep}
-              disabled={step === 1}
-            >
-              Go back
-            </CustomButton>
-            <CustomButton
-              onClick={() => {
-                handleNextStep();
-                step === 2 && handleFinish();
-              }}
-              disabled={step === totalSteps || isSubmitting}
-              className={`${step === 2 && "px-6"} ${
-                isSubmitting
-                  ? "bg-gray-200 border-none text-white cursor-not-allowed"
-                  : ""
-              }`}
-            >
-              {isSubmitting && <LoadingSpinner />}
-              {step === 2 ? "Finish" : "Continue"}
-            </CustomButton>
-          </div>
-        </section>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
--- src/components/misc/ChatbotCard.tsx
+++ src/components/misc/ChatbotCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Ellipsis } from "lucide-react";
+import Skeleton from "../ui/skeleton";
 
 const ChatbotCard = ({
   children,
@@ -20,24 +21,7 @@ const ChatbotCard = ({
             {children || "Chatbot One"}
           </h3>
         </div>
-        <div className="skeleton py-3 flex items-center justify-center flex-col gap-2">
-          <div className="animate-pulse space-y-4  p-4 bg-gray-100 w-[141px] h-[50px] rounded-lg flex-grow">
-            {/* Paragraph skeleton */}
-            <div className="space-y-2 ">
-              <div className="h-1 bg-white rounded w-full" />
-              <div className="h-1 bg-white rounded w-full" />
-              <div className="h-1 bg-white rounded w-full" />
-            </div>
-          </div>
-          <div className="animate-pulse space-y-4  p-4 bg-blue-50 w-[141px] h-[50px] rounded-lg justify-self-end ">
-            {/* Paragraph skeleton */}
-            <div className="space-y-2 ">
-              <div className="h-1 bg-blue-100 rounded w-full" />
-              <div className="h-1 bg-blue-100 rounded w-full" />
-              <div className="h-1 bg-blue-100 rounded w-full" />
-            </div>
-          </div>
-        </div>
+        <Skeleton />
       </div>
     </div>
   );
